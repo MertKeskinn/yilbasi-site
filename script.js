592aa848d6cc1b5f440a07bd507d0bdb67e32341
Mert Keskin <150377666+MertKeskinn@users.noreply.github.com>
2025-12-31 13:45:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ setInterval(() => {
   slides[index].classList.add("active");
 }, 4000);
 const images = [
-  "hero/1.jpg",
+  "hero/1.",
   "hero/2.jpg",
   "hero/3.jpg"
   "hero/4.jpg",
@@ -22,5 +22,6 @@ const images = [
   "hero/13.jpg",
   "hero/14.jpg"
 ];
+
 
 
